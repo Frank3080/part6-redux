8d5ba239c7b8ecfcd8bf414b779f7efaf70c4b83
Frank3080 <147672388+Frank3080@users.noreply.github.com>
2024-01-16 16:54:18 +0100

    6.24

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -9,18 +9,37 @@ const AnecdoteForm = () => {
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (anecdote) => {
-      const anecdotes = queryClient.getQueryData(["anecdotes"]);
-      queryClient.setQueryData(["anecdotes", anecdotes.concat(anecdote)]);
+      const currentAnecdotes = queryClient.getQueryData(["anecdotes"]) || [];
+      queryClient.setQueryData(["anecdotes"], [...currentAnecdotes, anecdote]);
+      queryClient.invalidateQueries(["anecdotes"]);
+      dispatch({ type: "set", payload: `"${anecdote.content}" Created.` });
+      setTimeout(() => dispatch({ type: "clear" }), 5000);
+    },
+    onError: (err) => {
+      console.log(err);
+      dispatch({
+        type: "set",
+        payload: "An error has ocurred creating that anecdote.",
+      });
+      setTimeout(() => dispatch({ type: "clear" }), 5000);
     },
   });
 
   const onCreate = (e) => {
     e.preventDefault();
     const content = e.target.anecdote.value;
+
+    if (content.length < 5) {
+      dispatch({
+        type: "set",
+        payload: "Anecdote content must be at least 5 characters long.",
+      });
+      setTimeout(() => dispatch({ type: "clear" }), 5000);
+      return;
+    }
+
     e.target.anecdote.value = "";
     newAnecdoteMutation.mutate({ content, votes: 0 });
-    dispatch({ type: "set", payload: `You created '${content}'` });
-    setTimeout(() => dispatch({ type: "clear" }), 5000);
   };
 
   return (
